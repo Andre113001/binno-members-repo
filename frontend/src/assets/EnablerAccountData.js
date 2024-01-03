b40e7a37c9d1286b486ee7b7fe73b254fd923c5e
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-01-03 22:53:25 +0800

    added object sample data for the account page

---
+++ frontend/src/assets/EnablerAccountData.js
@@ -0,0 +1,47 @@
+export const blog = [
+    {
+      id: 1,
+      title: "POST NO.1 CONTAINING A YOUTUBE VIDEO",
+      user: 'BU-CTCED',
+      userType: 'Startup Enabler',
+      category: "Blog",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis suscipit officia recusandae, eveniet quaerat assumendaid fugit, dignissimos maxime non natus placeat illo iusto!Sapiente dolorum id maiores dolores? Illum pariatur possimusquaerat ipsum quos molestiae rem aspernatur dicta tenetur. Suntplaceat tempora vitae enim incidunt porro fuga eaQuisque at tellus nec magna viverra euismod. Maecenas lobortis, nisl et sodales blandit, neque mi ullamcorper eros, id efficitur ex velit in ante. Suspendisse elementum congue lacus quis gravida. Nunc eu hendrerit risus. Quisque blandit, orci non maximus vulputate, ligula ante ornare enim, ac interdum lorem leo varius mauris. Aliquam non sem vel orci malesuada sagittis. Vivamus id ullamcorper tellus. In hac habitasse platea dictumst. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras et sapien a arcu feugiat mattis. Suspendisse sed magna quis turpis rhoncus venenatis.",
+      cover: "https://images.unsplash.com/photo-1541963463532-d68292c34b19?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8fA%3D%3D",
+      date: "APRIL 05, 2018",
+    },
+  
+    {
+      id: 2,
+      title: "JUPITER IS FULL OF WATERFALLS SO EARLY",
+      user: 'SILI DELI',
+      userType: 'Startup Enabler',
+      category: "Event",
+      desc: "Vestibulum suscipit nibh sed odio pharetra, condimentum lacinia nisi varius. Ut ac ornare diam. Suspendisse vitae quam risus. Nulla ullamcorper libero justo, vitae dictum est efficitur id. Aenean et lacinia arcu. Sed non dolor id risus dapibus congue. Quisque tincidunt vitae tellus ut malesuada. Nulla eleifend nibh a maximus consectetur. Duis et mi porta, bibendum leo non, fermentum enim. Vivamus sed diam suscipit, ultricies ipsum sed, efficitur dui. Sed pharetra purus at iaculis vehicula. Fusce pellentesque placerat ante, id elementum dolor maximus sed. Aenean viverra at lectus quis vehicula. Aenean at arcu odio. Etiam mauris justo, cursus finibus aliquam eget, fermentum nec nulla. Sed finibus viverra magna non sagittis. Mauris fringilla augue vel quam rhoncus, in tempor enim lacinia. Donec nibh velit, scelerisque id neque sit amet, ornare consectetur sem. Donec convallis nec justo id consequat. Donec sem leo, pulvinar sit amet nisi a, mollis gravida est. Duis porttitor eleifend cursus. Vivamus ac dapibus dolor. Nullam leo tellus, rhoncus sit amet tempor vel, malesuada vel augue. Morbi laoreet sem eget tortor pulvinar mattis. Maecenas commodo ultricies orci, ut blandit urna accumsan sit amet. Ut a porttitor orci, maximus feugiat turpis. In eu sodales orci, et volutpat felis. Quisque orci quam, gravida at nulla tempor, vulputate pretium magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent congue, magna vitae interdum blandit, arcu mauris placerat nulla, sit amet rhoncus justo risus in neque.",
+      cover: "https://images.pexels.com/photos/36029/aroni-arsa-children-little.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      date: "MARCH 15, 2018",
+    },
+];
+
+export const guide = [
+  {
+      id: 1,
+      title: 'How to Register a Startup',
+      img: "https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/RE2wSVH_RE4dchU:VP1-539x349?resMode=sharp2&op_usm=1.5,0.65,15,0&qlt=75",
+      date: 'November 20, 2023'
+  },
+];
+
+export const events = [
+  {
+      id: 1,
+      title: "Program Release",
+      desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
+      date: "MARCH 15, 2018",
+  },
+  {
+    id: 2,
+    title: "Program Release",
+    desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
+    date: "MARCH 15, 2018",
+},
+];
---
+++ frontend/src/assets/companyInfo.js
@@ -0,0 +1,13 @@
+export const companyInformation = 
+    {
+        id: 1,
+        user: 'BU-CTCED',
+        userType: 'Startup Enabler',
+        followerCount: '20000',
+        companyDescription: "The official page of Bicol University Center for Technology Commercialization and Enterprise Development (BU-CTCED)",
+        email: "bu-ctced@bicol-u.edu.ph",
+        contactNumber: "(052) 742 2587",
+        facebookPage: 'https://www.facebook.com/BUCTCED/',
+        companyLocation: 'HERRC Building, Bicol University East Campus, EMs Barrio 4500 Legazpi, Philippines'
+
+    }
