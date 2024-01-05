aac6d37cccee245dc83d113274d2a8c523e86728
Ted30 <teddymarcenaje@gmail.com>
2024-01-06 01:15:24 +0800

    added insecureAuth

--- database/db.js
+++ database/db.js
@@ -9,6 +9,7 @@ const db = mysql.createConnection({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
     database: process.env.MYSQL_DB,
+    insecureAuth: true, // Add this line to use older authentication method
 })
 
 db.connect((err) => {
