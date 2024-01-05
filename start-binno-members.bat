e8bed93fe861750eaf16c4c04bfdb037aa6b4a94
Teddy Marc Enaje <100709756+Teds30@users.noreply.github.com>
2024-01-06 00:02:34 +0800

    Delete start-binno-members.bat

--- start-binno-members.bat
+++
@@ -1,4 +0,0 @@
-wt new-tab -p PowerShell -d C:\Users\1130m\Desktop\BiNNO\binno-main\members --tabColor "#7da1db" --title "CMD"; ^
-new-tab -p PowerShell -d C:\Users\1130m\Desktop\BiNNO\binno-main\members\backend --tabColor "#edbd98" --title "Backend"; ^
-new-tab -p PowerShell -d C:\Users\1130m\Desktop\BiNNO\binno-main\members\frontend --tabColor "#c898ed" --title "Frontend"; ^
-new-tab -p PowerShell -d C:\Users\ --tabColor "#71996d" --title "MySQL"
