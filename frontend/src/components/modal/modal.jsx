81b18ff11d06c2f1910b85f4c05d596cb4d3ccc2
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-01-09 01:12:16 +0800

    Created a New textbox for the modal

--- frontend/src/components/modal/modal.jsx
+++ frontend/src/components/modal/modal.jsx
@@ -3,7 +3,7 @@ import './modal.css'
 import DropBox from '../dropbox/DropBox'
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import AddIcon from '@mui/icons-material/Add';
-
+import TextBox from './textBox';
 
 export default function Modal() {
   const [currentDate, setCurrentDate] = useState(new Date());
@@ -21,6 +21,15 @@ export default function Modal() {
     const dateYear = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
     return currentDate.toLocaleDateString(undefined, dateYear);
   }; 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const title = event.target.elements.title.value;
+    const category = event.target.elements.category.value;
+    const description = event.target.elements.description.value;
+    console.log('Form submitted:', { title, description, category});
+    toggleModal();
+  };
 
     const [modal, setModal] = useState(false);
     const toggleModal = () => {
@@ -59,15 +68,18 @@ export default function Modal() {
                       <button className='categoryButton'>Add to Category
                       </button>
                     </div>
-                    <input 
+                    <div className="texboxContainer">
+                      <TextBox />
+                      {/* <input 
                           type="text"
                           className='contentDetail'
                           placeholder='Write a Short description...'
-                        />
+                        /> */}
+                    </div>
                     <div className="dropboxContainer">
                       <DropBox />
                     </div>
-                    <button className='uploadButton' onClick={toggleModal}>  
+                    <button type="submit" className="uploadButton">  
                       Upload
                     </button>
                 </div>
--- frontend/src/components/modal/textBox.jsx
+++ frontend/src/components/modal/textBox.jsx
@@ -1,23 +1,31 @@
 import { useEffect, useRef, useState } from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { TextareaAutosize } from '@mui/base/TextareaAutosize';
 
 
-
-export default function textBox() {
-    const textAreaRef = useRef(null);
-    const [val, setVal] = useState("");
-    const handleChange = (e) => {
-        setVal(e.target.value);
-    }
-
-    useEffect(() => {
-        textAreaRef.current.style.height = "auto"
-        textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px";
-    }, [val])
-
+export default function TextBox() {
 
   return (
     <div>
-        
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 4, width: '1250px' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextareaAutosize
+            aria-label="empty textarea"
+            placeholder="Write a short description"
+            style={{ maxHeight: '250px' ,height: '150px',maxWidth:'1300px' ,width: '100%', 
+              margin: "20px", border:"rgb(241,241,241)", backgroundColor:"rgb(241,241,241)"}}
+            minRows={4}
+          />
+        </div>
+    </Box>
     </div>
   )
 }
