04cf64f157e9fa9777ebcdef3e10c110482b4409
Andre113001 <1130marcusa@gmail.com>
2024-01-21 03:10:32 +0800

    Image and member controller changes

--- api/controllers/imageController.js
+++ api/controllers/imageController.js
@@ -31,7 +31,7 @@ const moveFileToDirectory = (file, destinationDirectory) => {
 const getImage = async (req, res) => {
     const { file_path } = req.body
 
-    const imgPath = path.join(__dirname, `../../public/img/${file_path}`)
+    const imgPath = file_path;
     try {
         const imageBlob = getImageBlob(imgPath)
 
