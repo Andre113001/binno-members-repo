e42c2ab706838a4ff7b6f8573bb0b4994debeb7d
alexxShandsome <algilbertgomez@gmail.com>
2024-02-13 11:13:47 +0800

    refactor: new database query for newsletterController.js

--- api/controllers/newsletterController.js
+++ api/controllers/newsletterController.js
@@ -10,37 +10,66 @@ const subscribe = async (req, res) => {
 
     // Checker
     try {
-        db.query("SELECT email_address, email_subscribe FROM email_i WHERE email_address = ?", [sanitizeId(email)], (err, result) => { // Check if email already is subscribed
+        // Check if email already is subscribed
+        const verifyEmailQuery = "SELECT email_address, email_subscribe FROM email_i WHERE email_address = ?";
+        // NOTE: new query for the new database - AL
+        // const verifyEmailQuery = "SELECT email_address, subscribe FROM email WHERE email_address = ?";
+        db.query(verifyEmailQuery, [sanitizeId(email)], (err, result) => {
             if (err) {
-                res.json({error: err});
+                res.json({ error: err });
             }
-            
+
             if (result.length > 0) {
+                // NOTE: change condition to - AL
+                // if (result[0].subscribe === 0)
                 if (result[0].email_subscribe === 0) {
-                    db.query("UPDATE email_i SET email_subscribe = 1 WHERE email_address = ?", [sanitizeId(email)], (err, result) => {
+                    const updateSubscriptionQuery = "UPDATE email_i SET email_subscribe = 1 WHERE email_address = ?";
+                    // NOTE: new query for the new database - AL
+                    // const updateSubscriptionQuery = "UPDATE email SET subscribe = 1 WHERE email_address = ?";
+                    db.query(updateSubscriptionQuery, [sanitizeId(email)], (err, result) => {
                         if (err) {
                             res.json(err);
                         }
-            
+
                         if (result.affectedRows > 0) {
-                            res.json({result: "Successfully Re-Subscribe"});
+                            res.json({ result: "Successfully Re-Subscribe" });
                         } else {
-                            res.json({result: "Attempt to Unsubscribe failed "});
+                            res.json({ result: "Attempt to Unsubscribe failed " });
                         }
                     })
                 } else {
-                    res.json({result: "Already Subscribe"});
+                    res.json({ result: "Already Subscribe" });
                 }
+
+                // if no email found then insert to email table
             } else {
-                db.query("INSERT INTO email_i (email_id, email_datecreated, email_address, email_user_type) VALUES (?, NOW(), ?,  ?)", [id, email, "subscriber"], (insertErr, insertRes) => {
+                const insertEmailQuery = `
+                    INSERT INTO email_i (
+                        email_id,
+                        email_datecreated,
+                        email_address,
+                        email_user_type
+                    )
+                    VALUES (?, NOW(), ?,  ?)
+                `;
+                // NOTE: new query for the new database - AL
+                // const insertEmailQuery = `
+                //     INSERT INTO email (
+                //         email_id,
+                //         date_created,
+                //         email_address,
+                //     )
+                //     VALUES (?, NOW(), ?)
+                // `;
+                db.query(insertEmailQuery, [id, email, "subscriber"], (insertErr, insertRes) => {
                     if (insertErr) {
-                        res.json({insertError: insertErr});
+                        res.json({ insertError: insertErr });
                     }
 
                     if (insertRes.affectedRows > 0) {
-                        res.json({result: "Email Added"});
+                        res.json({ result: "Email Added" });
                     } else {
-                        res.json({result: "Email Not Added"});
+                        res.json({ result: "Email Not Added" });
                     }
                 })
             }
@@ -55,19 +84,22 @@ const unsubscribe = async (req, res) => {
     const { email_id } = req.query;
 
     try {
-        db.query("UPDATE email_i SET email_subscribe = 0 WHERE email_id = ?", [email_id], (err, result) => {
+        const unsubscribeQuery = "UPDATE email_i SET email_subscribe = 0 WHERE email_id = ?";
+        // NOTE: new query for the new database - AL
+        // const unsubscribeQuery = "UPDATE email SET subscribe = 0 WHERE email_id = ?";
+        db.query(unsubscribeQuery, [email_id], (err, result) => {
             if (err) {
                 res.json(err);
             }
 
             if (result.affectedRows > 0) {
-                res.json({result: "Successfully Unsubscribe"});
+                res.json({ result: "Successfully Unsubscribe" });
             } else {
-                res.json({result: "Attempt to Unsubscribe failed "});
+                res.json({ result: "Attempt to Unsubscribe failed " });
             }
         })
     } catch (error) {
-        res.json({error: error});
+        res.json({ error: error });
     }
 }
 
