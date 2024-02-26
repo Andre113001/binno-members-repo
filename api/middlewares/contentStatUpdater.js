80e7532e3202ee546ba503f3260412b99f05cfb9
alexxShandsome <algilbertgomez@gmail.com>
2024-02-26 16:34:29 +0800

    feat: content stat deductor middleware

--- api/middlewares/contentStatUpdater.js
+++ api/middlewares/contentStatUpdater.js
@@ -2,7 +2,8 @@ const db = require('../../database/db');
 
 /**
 * Create or Update a Content Stat based on the current date and the type of content
-* @param {content_type} string - should be "blog", "guide", "post", or "event"
+* @param {string} content_type - should be "blog", "guide", "post", or "event"
+* @returns {Promise<Array>} A Promise that resolves to an array containing the update or create result or rejects with an error.
 */
 const updateContentStat = async (content_type) => {
 	return new Promise((resolve, reject) => {
@@ -45,6 +46,42 @@ const updateContentStat = async (content_type) => {
 	});
 }
 
+/**
+* Deduct a Content Stat based on the current date and the type of content
+* @param {date} stat_date - date of the content to be deducted
+* @param {string} content_type - should be "blog", "guide", "post", or "event"
+* @returns {Promise<Array>} A Promise that resolves to an array containing the deduction result or rejects with an error.
+*/
+const deductContentStat = async (stat_date, content_type) => {
+	return new Promise((resolve, reject) => {
+		const checkContentStatDateQuery = `
+			 SELECT * FROM content_stat WHERE
+			 stat_date = ? AND content_type = ?
+		`;
+		db.query(checkContentStatDateQuery, [stat_date, content_type], (checkError, checkResult) => {
+			if (checkError) {
+				console.error(checkError);
+				reject(checkError);
+			}
+			else if (checkResult.length > 0) {
+				const deductContentStatQuery = `
+					UPDATE content_stat SET
+					stat_count = stat_count - 1
+					WHERE stat_date = ? AND content_type = ?
+				`;
+				db.query(deductContentStatQuery, [stat_date, content_type], (deductError, deductResult) => {
+					if (deductError) {
+						console.error(deductError);
+						reject(deductError);
+					}
+					else resolve(deductResult);
+				});
+			}
+		})
+	});
+}
+
 module.exports = {
-	updateContentStat
+	updateContentStat,
+	deductContentStat
 };
