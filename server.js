b98fb1f3725590a6aa06a642c2dcbf90bf598940
Andre113001 <1130marcusa@gmail.com>
2024-03-04 12:01:43 +0800

    asdad

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config()
 const app = express();
 const port = process.env.PORT;
 const corsOptions = {
-  origin: 'https://member.binnostartup.site',
+  origin: '*',
   // Add additional origins as needed
 };
 
