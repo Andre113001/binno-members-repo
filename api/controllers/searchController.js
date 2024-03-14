77279a5dc3596abb63f4be2ed9c2cf590cffa306
Andre113001 <1130marcusa@gmail.com>
2024-03-14 14:57:24 +0800

    search fixed on flag and filter by company and enabler

--- api/controllers/searchController.js
+++ api/controllers/searchController.js
@@ -4,7 +4,43 @@ const searchBlog = async (req, res) => {
     const { search_term } = req.body;
 
     try {
-        db.query("SELECT blog_i.*, setting_institution FROM blog_i INNER JOIN member_settings ON blog_i.blog_author = member_settings.setting_memberId WHERE setting_institution LIKE ? OR blog_title LIKE ? AND blog_flag = 1", [`%${search_term}%`, `%${search_term}%`], (searchError, searchResult) => {
+        db.query("SELECT blog_i.*, setting_institution FROM blog_i INNER JOIN member_settings ON blog_i.blog_author = member_settings.setting_memberId WHERE (setting_institution LIKE ? OR blog_title LIKE ?) AND blog_flag = 1", [`%${search_term}%`, `%${search_term}%`], (searchError, searchResult) => {
+            if (searchError) {
+                console.error('Error executing MySQL query:', searchError);
+                res.status(500).json({ error: 'Error executing MySQL query' });
+            } else {
+                res.json(searchResult);
+            }
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Server error' });
+    }
+};
+
+const searchCompanyBlogs = async (req, res) => {
+    const { search_term } = req.body;
+
+    try {
+        db.query("SELECT blog_i.*, member_type, setting_institution FROM blog_i INNER JOIN member_settings ON blog_i.blog_author = member_settings.setting_memberId INNER JOIN member_i ON blog_i.blog_author = member_i.member_id WHERE (setting_institution LIKE ? OR blog_title LIKE ?) AND member_type = 1 AND blog_flag = 1", [`%${search_term}%`, `%${search_term}%`], (searchError, searchResult) => {
+            if (searchError) {
+                console.error('Error executing MySQL query:', searchError);
+                res.status(500).json({ error: 'Error executing MySQL query' });
+            } else {
+                res.json(searchResult);
+            }
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Server error' });
+    }
+};
+
+const searchEnablerBlogs = async (req, res) => {
+    const { search_term } = req.body;
+
+    try {
+        db.query("SELECT blog_i.*, member_type, setting_institution FROM blog_i INNER JOIN member_settings ON blog_i.blog_author = member_settings.setting_memberId INNER JOIN member_i ON blog_i.blog_author = member_i.member_id WHERE (setting_institution LIKE ? OR blog_title LIKE ?) AND member_type = 2 AND blog_flag = 1", [`%${search_term}%`, `%${search_term}%`], (searchError, searchResult) => {
             if (searchError) {
                 console.error('Error executing MySQL query:', searchError);
                 res.status(500).json({ error: 'Error executing MySQL query' });
@@ -57,6 +93,8 @@ const searchGuides = async (req, res) => {
 
 module.exports = {
     searchBlog,
+    searchCompanyBlogs,
+    searchEnablerBlogs,
     searchEvent,
     searchGuides
 }
--- api/routes/searchRoute.js
+++ api/routes/searchRoute.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const searchController = require('../controllers/searchController');
 
 router.post('/blog', searchController.searchBlog);
+router.post('/blog/company', searchController.searchCompanyBlogs);
+router.post('/blog/enabler', searchController.searchEnablerBlogs);
 router.post('/event', searchController.searchEvent);
 router.post('/guide', searchController.searchGuides);
 
